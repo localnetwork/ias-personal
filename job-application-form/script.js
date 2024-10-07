b64705d6db50e6f37e91d3e81a78033827417d7e
localnetwork <diomenikep@gmail.com>
2024-10-07 09:46:26 +0800

    chore: job app form

--- job-application-form/script.js
+++ job-application-form/script.js
@@ -60,11 +60,19 @@ function validateField(field) {
 
     if (field.files.length > 0) {
       const file = field.files[0];
+      const files = field.files;
       const validExtensions = [
         "application/pdf",
         "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
       ];
-      if (!validExtensions.includes(file.type)) {
+
+      const fileTypes = Array.from(files).map((item) => item.type);
+
+      const isInvalid = fileTypes.some(
+        (type) => !validExtensions.includes(type)
+      );
+
+      if (isInvalid) {
         isValid = false;
         const message = document.createElement("div");
         message.className = "validation-message";
@@ -84,20 +92,22 @@ function validateField(field) {
         previewNoUpload.style.display = "flex";
       } else {
         // Insert filename and file size inside .preview-upload
-        previewUpload.innerHTML = `Filename: ${file.name}, Size: ${(
-          file.size / 1024
-        ).toFixed(2)} KB`;
+        previewUpload.innerHTML = Array.from(files)
+          .map((item) => {
+            return `<div class="preview-file">
+            <div class="preview-file-wrapper">
+              <div class="title">${item.name}</div>
+              <div class="size">${(item.size / 1024).toFixed(2)} KB</div>
+              <div class="file-type">${item?.type.split("/")[1]}</div>
+            </div>
+            </div>`;
+          })
+          .join("");
 
         // Create and append remove button
         const removeButton = document.createElement("button");
-        removeButton.innerText = "Remove";
-        removeButton.style.marginLeft = "10px";
-        removeButton.style.backgroundColor = "#9A0C16"; // Set background color
-        removeButton.style.color = "white"; // Set text color
-        removeButton.style.padding = "5px 10px"; // Adjust padding
-        removeButton.style.border = "none"; // Remove border
-        removeButton.style.borderRadius = "3px"; // Add border radius
-        removeButton.style.cursor = "pointer"; // Change cursor to pointer
+        removeButton.classList.add("remove-file");
+        removeButton.innerText = "Remove Files";
         removeButton.addEventListener("click", function (e) {
           e.preventDefault(); // Prevent form submission
           field.value = ""; // Clear the file input
@@ -107,7 +117,7 @@ function validateField(field) {
         previewUpload.appendChild(removeButton);
 
         // Show preview-upload and hide preview-noupload
-        previewUpload.style.display = "block";
+        previewUpload.style.display = "flex";
         previewNoUpload.style.display = "none";
       }
     } else {
@@ -125,9 +135,11 @@ function validateFields() {
   let isValid = true;
 
   for (let field of formFields) {
-    if (field.tagName === "INPUT" || field.tagName === "TEXTAREA") {
-      if (!validateField(field)) {
-        isValid = false;
+    if (field?.getAttribute("data-required") == "true") {
+      if (field.tagName === "INPUT" || field.tagName === "TEXTAREA") {
+        if (!validateField(field)) {
+          isValid = false;
+        }
       }
     }
   }
@@ -137,15 +149,17 @@ function validateFields() {
 
 // Add event listeners to fields to validate on change/input
 for (let field of formFields) {
-  if (field.tagName === "INPUT" || field.tagName === "TEXTAREA") {
-    field.addEventListener("input", function () {
-      validateField(field);
-    });
-
-    if (field.type === "file") {
-      field.addEventListener("change", function () {
+  if (field?.getAttribute("data-required") == "true") {
+    if (field.tagName === "INPUT" || field.tagName === "TEXTAREA") {
+      field.addEventListener("input", function () {
         validateField(field);
       });
+
+      if (field.type === "file") {
+        field.addEventListener("change", function () {
+          validateField(field);
+        });
+      }
     }
   }
 }
